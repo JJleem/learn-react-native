4e7b7844c13f984da27162292acfbd1062b375a9
JJleem <leemjaejun@gmail.com>
2024-09-05 18:28:22 +0900

    240905

---
+++ WorkHardTravelHardApp/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.btnText}>Work</Text>
+        <Text style={styles.btnText}>Travel</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
+  },
+  btnText: {
+    color: "white",
+    fontSize: 38,
+    fontWeight: "600",
+  },
+});
