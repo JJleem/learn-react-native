9f58276a654abc122678419c33d14d2162e3b376
JJleem <leemjaejun@gmail.com>
2024-09-09 18:14:59 +0900

    240909

---
+++ naverMobileTestWithMolt/app/(tabs)/test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function Test() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Hello Expo native World
+    </View>
+  );
+}
---
+++ naverMobileTestWithMolt/app/testfile/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>testfile</Text>
+      {/* <Tabs style={{}}>
+        <Tabs.Screen name="test" options={{ title: "test" }}></Tabs.Screen>
+      </Tabs> */}
+    </View>
+  );
+}
