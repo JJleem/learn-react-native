8b502a2410069044084987cc40809730c8392608
JJleem <leemjaejun@gmail.com>
2024-09-20 18:14:17 +0900

    240920

--- mobileTest/App.tsx
+++ mobileTest/App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -20,107 +19,109 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: "#ffffff", // 탭 바 배경색
-            borderTopWidth: 1,
-            borderTopColor: "#ccc", // 탭 바 경계선 색상
-            marginBottom: 5,
-            paddingTop: 5,
-          },
-          tabBarActiveTintColor: "#1d1d1d", // 활성화된 탭 색상
-          tabBarInactiveTintColor: "#c9c9c9", // 비활성화된 탭 색상
-          tabBarLabelStyle: {
-            fontSize: 9, // 탭 레이블 글씨 크기
-          },
-        }}
-      >
-        <Tab.Screen
-          name="HomeStack"
-          component={HomeStack}
-          options={{
-            headerShown: false,
-            tabBarLabel: "홈",
-            tabBarIcon: ({ color, focused }) => (
-              <Ionicons
-                name={focused ? "home" : "home-outline"}
-                size={24}
-                color={color}
-              />
-            ),
+    <>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: {
+              backgroundColor: "#ffffff", // 탭 바 배경색
+              borderTopWidth: 1,
+              borderTopColor: "#ccc", // 탭 바 경계선 색상
+              marginBottom: 5,
+              paddingTop: 5,
+            },
+            tabBarActiveTintColor: "#1d1d1d", // 활성화된 탭 색상
+            tabBarInactiveTintColor: "#c9c9c9", // 비활성화된 탭 색상
+            tabBarLabelStyle: {
+              fontSize: 9, // 탭 레이블 글씨 크기
+            },
           }}
-        />
-        <Tab.Screen
-          name="ReservationStackFnc"
-          component={ReservationStackFnc}
-          options={{
-            headerShown: false,
-            tabBarLabel: "나의 예약",
-            tabBarIcon: ({ color, focused }) => (
-              <Ionicons
-                name={focused ? "calendar-sharp" : "calendar-outline"}
-                size={24}
-                color={color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="ScoreStackFnc"
-          component={ScoreStackFnc}
-          options={{
-            headerShown: false,
-            tabBarLabel: "스코어",
-            tabBarIcon: ({ color, focused }) => (
-              <Ionicons
-                name={focused ? "clipboard" : "clipboard-outline"}
-                size={24}
-                color={color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="GiftStackFnc"
-          component={GiftStackFnc}
-          options={{
-            headerShown: false,
-            tabBarLabel: "혜택",
-            tabBarIcon: ({ color, focused }) => (
-              <Ionicons
-                name={focused ? "gift" : "gift-outline"}
-                size={24}
-                color={color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="MyAccountStackFnc"
-          component={MyAccountStackFnc}
-          options={{
-            headerShown: false,
-            tabBarLabel: "MY",
-            tabBarIcon: ({ color, focused }) => (
-              <Ionicons
-                name={focused ? "person" : "person-outline"}
-                size={24}
-                color={color}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              headerShown: false,
+              tabBarLabel: "홈",
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons
+                  name={focused ? "home" : "home-outline"}
+                  size={24}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="ReservationStackFnc"
+            component={ReservationStackFnc}
+            options={{
+              headerShown: false,
+              tabBarLabel: "나의 예약",
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons
+                  name={focused ? "calendar-sharp" : "calendar-outline"}
+                  size={24}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="ScoreStackFnc"
+            component={ScoreStackFnc}
+            options={{
+              headerShown: false,
+              tabBarLabel: "스코어",
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons
+                  name={focused ? "clipboard" : "clipboard-outline"}
+                  size={24}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="GiftStackFnc"
+            component={GiftStackFnc}
+            options={{
+              headerShown: false,
+              tabBarLabel: "혜택",
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons
+                  name={focused ? "gift" : "gift-outline"}
+                  size={24}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MyAccountStackFnc"
+            component={MyAccountStackFnc}
+            options={{
+              headerShown: false,
+              tabBarLabel: "MY",
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons
+                  name={focused ? "person" : "person-outline"}
+                  size={24}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
--- mobileTest/screens/HomeStack/HomeScreen.tsx
+++ mobileTest/screens/HomeStack/HomeScreen.tsx
@@ -1,8 +1,19 @@
-import React from "react";
-import { Button, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Animated,
+  Button,
+  Image,
+  Keyboard,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../StackFnc/Stack";
-
+import styled from "styled-components/native";
 type HomeScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
   "Home"
@@ -16,13 +27,178 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const goToDetails = () => {
     navigation.navigate("Details", { itemId: 0, title: "상세 화면 제목" });
   };
+  const [isClicked, setIsClicked] = useState({
+    golf: true,
+    reservation: false,
+  });
+  const handleHeader = (key: "golf" | "reservation") => {
+    setIsClicked((prev) => ({
+      ...prev,
+      golf: key === "golf" ? !prev.golf : false,
+      reservation: key === "reservation" ? !prev.reservation : false,
+    }));
+  };
+  const [visible, setVisible] = useState(false);
+  const [translateY] = useState(new Animated.Value(400));
+
+  const toggleSearch = () => {
+    Keyboard.dismiss();
+
+    setVisible(!visible);
+    Animated.timing(translateY, {
+      toValue: visible ? 300 : 0, // 위로 올라오거나 내려감
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>홈 화면</Text>
-      <Button title="상세 화면으로 이동" onPress={goToDetails} />
-    </View>
+    <>
+      <HeaderView>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 20,
+            alignContent: "center",
+          }}
+        >
+          <HeaderTouch
+            isClick={isClicked.golf}
+            onPress={() => handleHeader("golf")}
+          >
+            <HeaderText isClick={isClicked.golf}>골프예약</HeaderText>
+          </HeaderTouch>
+          <HeaderTouch
+            isClick={isClicked.reservation}
+            onPress={() => handleHeader("reservation")}
+          >
+            <HeaderText isClick={isClicked.reservation}>예약+</HeaderText>
+          </HeaderTouch>
+        </View>
+      </HeaderView>
+      <TextInputView>
+        <HeaderTextInput
+          placeholder="지역,골프장명,키워드 검색"
+          onChangeText={(text) => console.log(text)}
+          secureTextEntry={false}
+          keyboardType="default"
+          maxLength={100}
+          multiline={false}
+          onFocus={toggleSearch}
+        />
+        <SearchImg
+          source={require("../../assets/icon/search.png")}
+          resizeMode="contain"
+        />
+      </TextInputView>
+
+      <MainView>
+        {/* <Text>홈 화면dd</Text>
+        <Button title="상세 화면으로 이동" onPress={goToDetails} /> */}
+      </MainView>
+      {visible && (
+        <ToggleModalView
+          style={{ elevation: 5 }}
+          onPress={() => setVisible(!visible)}
+        >
+          <ToggleView style={[{ transform: [{ translateY }] }]}>
+            <TextInputView>
+              <HeaderTextInput
+                placeholder="지역,골프장명,키워드 검색"
+                onChangeText={(text) => console.log(text)}
+                secureTextEntry={false}
+                keyboardType="default"
+                maxLength={100}
+                multiline={false}
+                onFocus={toggleSearch}
+              />
+              <SearchImg
+                source={require("../../assets/icon/search.png")}
+                resizeMode="contain"
+              />
+            </TextInputView>
+            <TextInputView></TextInputView>
+          </ToggleView>
+        </ToggleModalView>
+      )}
+    </>
   );
 };
 
 export default HomeScreen;
+
+type isClick = {
+  isClick: boolean;
+};
+
+const MainView = styled.View`
+  flex: 10;
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+`;
+const HeaderView = styled.View`
+  flex: 1;
+  align-items: start;
+  background: #eee;
+  flex-direction: column;
+  justify-content: center;
+  padding: 25px 25px;
+  gap: 27px;
+  padding-top: 80px;
+`;
+const TextInputView = styled.View`
+  flex: 1;
+  background: #eee;
+  justify-content: center;
+  padding: 25px 25px;
+  padding-top: 0px;
+  gap: 27px;
+  position: relative;
+`;
+const HeaderText = styled.Text<isClick>`
+  color: ${(props) => (props.isClick ? "#141414" : "#aaa")};
+  font-size: 30px;
+  font-weight: 600;
+  letter-spacing: -1.5px;
+`;
+const HeaderTouch = styled.TouchableOpacity<{ isClick: boolean }>`
+  padding-bottom: ${(props) => (props.isClick ? "10px" : "0")};
+  border-bottom-width: ${(props) => (props.isClick ? "3px" : "0")};
+  border-bottom-color: ${(props) =>
+    props.isClick ? "#141414" : "transparent"};
+`;
+const HeaderTextInput = styled.TextInput`
+  padding: 10px;
+  padding-left: 48px;
+  flex: 1;
+  border-radius: 5px;
+  background-color: #fafafa;
+`;
+const SearchImg = styled.Image`
+  position: absolute;
+  left: 35px;
+  transform: translateY(-10px);
+  top: 50%;
+  width: 24px;
+  height: 24px;
+`;
+const ToggleModalView = styled.TouchableOpacity`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  background-color: rgba(0, 0, 0, 0.5);
+  justify-content: flex-end;
+  z-index: 1000;
+`;
+
+const ToggleView = styled(Animated.View)`
+  background-color: white;
+  padding: 20px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  height: 80%;
+`;
